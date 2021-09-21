8d0b3e97879d4d797de7c79981b3faa4a4fe9333
PGL50 <84110521+PGL50@users.noreply.github.com>
2021-09-21 17:51:46 -0500

    Polygons

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -1,7 +1,6 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -26,7 +25,7 @@ let baseMaps = {
 let map = L.map('mapid', {
   center: [43.7, -79.3],
   zoom: 11,
-  layers: [satteliteStreets]
+  layers: [streets]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
@@ -39,9 +38,24 @@ let torontoHoods = "https://raw.githubusercontent.com/PGL50/Mapping_Earthquakes/
 d3.json(torontoHoods).then(function(data) {
   console.log(data) ;
 
+  // Create a style for the lines.
+let myStyle = {
+  color: "blue",
+  weight: 1,
+  fillColor: 'yellow',
+}
+
+
 // Creating a GeoJSON layer with the retrieved data.
-L.geoJson(data).addTo(map);
+L.geoJson(data, {
+  style: myStyle,
+  onEachFeature: function(feature, layer) {
+    layer.bindPopup("<h3> Neighborhood: " + feature.properties.AREA_NAME + "</h3>")  ;
+  }
+  }).addTo(map);
 });
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+
